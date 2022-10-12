a2ac6765d2f06c48860e16c287b064295a63a82a
Kseniya Kamneva <kseniya5390@gmail.com>
2022-10-13 01:43:14 +0300

    Add seventh homework

---
+++ Homework 7/Program.cs
@@ -0,0 +1,139 @@
+﻿// Задача 47
+
+Console.WriteLine("Введите количество строк: ");
+int str = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[str, cols];
+FillArray(array);
+Print(array);
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10));
+        }
+    }
+}
+
+void Print(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+//Задача 50
+
+Console.WriteLine("Введите количество строк: ");
+int str = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [str,cols];
+Console.WriteLine("Введите порядковый номер элемента: ");
+int num = Convert.ToInt32(Console.ReadLine());
+FillArray(array);
+Print(array);
+
+if (str * cols < num)
+{
+    Console.WriteLine("Элемент не найден.");
+}
+else
+{
+    int pos = (str * cols) - num;
+    int i = 0;
+    while (pos > str)
+    {
+        pos = pos - str;
+        i++;
+    }
+    Console.WriteLine($"Значение элемента {num} равно {array[str - i - 1, cols - pos -1]}");
+}
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-10, 10);
+            }   
+        }
+}
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
+
+//Задача 52
+
+Console.WriteLine("Введите количество строк: ");
+int str = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [str,cols];
+FillArray(array);
+
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        avarage = (avarage + array[i, j]);
+    }
+    avarage = avarage / str;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+Print(array);
+
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void Print(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
